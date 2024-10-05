b40941edeed8ab5e132fe2901d4eee3b0be40ba6
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-05 11:28:27 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Header from './Header/header';
 import './App.css';
+import CategoryList from './Category/categoryList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper bg-fadad-dark'>
+      <Header></Header>
+      <CategoryList></CategoryList>
+
     </div>
   );
 }
