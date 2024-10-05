8c34a83b4d072cdd2bf6ef68f4f8c8caefd1e06e
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-05 11:30:08 +0330

    Add files via upload

---
+++ src/Header/header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+const Header =()=>{
+    return(
+        <header className='bg-size-cover bg-position-center py-5'>
+            <div className="container">
+                <h1 className='text-start text-light text-uppercase'>cafe menu </h1>
+            </div>
+        </header>
+    )
+}
+export default Header;
