b8c643acaa0cdedfa06225b5a5abc63732834326
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-07 13:57:34 +0330

    Update categoryList.jsx

--- src/Category/categoryList.jsx
+++ src/Category/categoryList.jsx
@@ -2,38 +2,57 @@ import { useEffect, useState } from "react";
 import axios from "../axios";
 import './cat.css'
 
+import Loding from "../Loding/loding";
 
 
-const CategoryList=()=>{
+const CategoryList=({filterItems})=>{
+    const [lodingg,upLodnig]=useState(true)
     const [categories,setcategories]=useState([]);
    useEffect(()=>{
     const feachData= async ()=>{
     const response=await axios.get('/FoodCategory/categories')
     setcategories(response.data)
+    upLodnig(false)
     }  
     feachData()
 },[])
 
     
-    return(
-        <nav className="container mt-n5">
-            <div className="bg-dark rounded-3 shadow-1g py-4 insize">
-                <ul className="nav">
-                    <li className="nav-item">
-                        <a href="/#" className="nav-link">
-                            hame
+const Renderr=()=>{
+    if(lodingg)
+    {
+           return(<Loding theme={"primary"}/>)
+    }
+
+return(
+<ul className="nav">
+<li className="nav-item" onClick={() => filterItems()}>
+                        <a className="nav-link" href="#">
+                            همه ی فست فودها
                         </a>
                     </li>
                     {
-                        categories.map(category=>(
-                            <li className="nav-item" key={category.id}>
-                                <a className="nav-link" href="/#">
+                        categories.map(category => (
+                            <li className="nav-item" key={category.id} onClick={() => filterItems(category.id)}>
+                                <a className="nav-link" href="#">
                                     {category.name}
                                 </a>
                             </li>
                         )  )
                     }
                 </ul>
+)
+}
+    return(
+        <nav className="container mt-n5">
+            <div className="bg-dark rounded-3 shadow-1g py-4 insize">
+                
+                    
+                       {Renderr()}
+                       
+                    
+                
+                
             </div>
         </nav>
     )
