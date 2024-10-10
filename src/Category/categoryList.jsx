162510e739c395a8a96ff53f13e206aac2bb66c4
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-10 21:42:12 +0330

    Update categoryList.jsx

--- src/Category/categoryList.jsx
+++ src/Category/categoryList.jsx
@@ -1,49 +1,55 @@
-import { useEffect, useState } from "react";
-import axios from "../axios";
+
+
 import './cat.css'
+import SearchBar from "../Searchbar/searchbar";
 
 import Loding from "../Loding/loding";
+import useAxios from "../useAxios";
 
 
-const CategoryList=({filterItems})=>{
-    const [lodingg,upLodnig]=useState(true)
-    const [categories,setcategories]=useState([]);
-   useEffect(()=>{
-    const feachData= async ()=>{
-    const response=await axios.get('/FoodCategory/categories')
-    setcategories(response.data)
-    upLodnig(false)
-    }  
-    feachData()
-},[])
+
+
+const CategoryList=({filterItems,searchItem})=>{
+const[categories, , loding]=useAxios({
+    url:'/FoodCategory/categories'
+})
+
+
+   
 
     
 const Renderr=()=>{
-    if(lodingg)
+    if(loding)
     {
            return(<Loding theme={"primary"}/>)
     }
 
 return(
+    <div className="p3-3 w-100 d-flex aligen-item-center justify-content-between gap-5">
 <ul className="nav">
 <li className="nav-item" onClick={() => filterItems()}>
-                        <a className="nav-link" href="#">
-                            همه ی فست فودها
+                        <a className="nav-link" href="/#">
+                            all  
                         </a>
                     </li>
                     {
                         categories.map(category => (
                             <li className="nav-item" key={category.id} onClick={() => filterItems(category.id)}>
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="/#">
                                     {category.name}
                                 </a>
                             </li>
                         )  )
-                    }
+                    };
                 </ul>
+              <SearchBar searchItem={searchItem} >
+               
+              </SearchBar>
+                </div>
 )
 }
     return(
+        
         <nav className="container mt-n5">
             <div className="bg-dark rounded-3 shadow-1g py-4 insize">
                 
